e63ef843140a1d37804451fcff5ade518527b52c
EC2 Default User <ec2-user@ip-172-31-26-244.us-east-2.compute.internal>
2018-07-04 11:01:37 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+/*
+//CRUD
+Route::post('messages','MessagesController@store')->name('messages.store');
+Route::put('messages/{id}','MessagesController@update')->name('messages.update');
+Route::delete('messages/{id}','MessagesController@destroy')->name('messages.destroy');
+
+//index
+Route::get('messages','MessagesController@incex')->name('messages.index');
+
+//create
+Route::get('messages/create','MessagerController@create')->name('messages.create');
+
+//show
+Route::get('messages/{id}','MessagesController@show')->name('messages.show');
+
+//edit
+Route::get('messages/{id}/edit','MessagesController@edit')->name('messages.edit');
+*/
+
+Route::get('/', 'MessagesController@index');
+Route::resource('messages', 'MessagesController');
