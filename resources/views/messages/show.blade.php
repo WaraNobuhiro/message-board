c9e8358ae5086564886b101c80214ee3447539db
EC2 Default User <ec2-user@ip-172-31-26-244.us-east-2.compute.internal>
2018-07-13 01:13:20 +0000

    add column

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 
     <h1>id={{$message->id}}のメッセージ詳細ページ</h1>
-    <p>{{$message->content}}</p>
+    <p>タイトル: {{ $message->title }}</p>
+    <p>メッセージ:{{$message->content}}</p>
 
     {!! link_to_route('messages.edit', 'このメッセージを編集', ['id' => $message->id]) !!}
     {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
